62fc66a03024f4b09fa72c68f2206738639d3af7
Jose Rossi <jrossi@altimetrik.com>
2020-01-08 10:31:18 -0300

    Updates

---
+++ precache-manifest.7f36e45e702084bd8707ecc2118b9131.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928dc2b6bf4e55ea493ad62ddabf7bc8",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1853da9d290fc064b8d4",
+    "url": "/monster-rolodex/static/css/main.8bab8f05.chunk.css"
+  },
+  {
+    "revision": "a0cf21bc00567dad5428",
+    "url": "/monster-rolodex/static/js/2.a2692686.chunk.js"
+  },
+  {
+    "revision": "1853da9d290fc064b8d4",
+    "url": "/monster-rolodex/static/js/main.e73e1fe4.chunk.js"
+  },
+  {
+    "revision": "f014c67a9d6c4b8b0c20",
+    "url": "/monster-rolodex/static/js/runtime-main.5c39f89c.js"
+  }
+]);
