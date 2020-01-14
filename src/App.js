bdda6b9ad6d046f292353c02d2d69e4de4ff462b
Jose Rossi <jrossi@altimetrik.com>
2020-01-14 17:26:33 -0300

    package

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import "./App.css";
 import {CardList} from "./components/card-list/card-list.component";
 import {SearchBox} from "./components/searchbox/searchbox.component";
+import {MaximumDate, MinimumDate, QuarterOfYear, DifferenceBetweenDates, DayOfTheWeek, AddDays} from "./components/datesHandler/dateHandler.component";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       monsters: [],
-      searchField: ""
+      searchField: "",
+      datesArray: [ new Date("2011/01/01"), new Date("2011/04/01"), new Date("2012/02/11"), new Date("2012/05/24"), new Date("2014/12/24")],
+      dateQuarter: (new Date("2017/03/28")),
+      daysToBeAdded: 7
     };
   }
   componentDidMount(){
@@ -20,10 +24,17 @@ class App extends Component {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
-    )
+    );
     return (
       <div className="App">
         <h1>Monster's Rolodex</h1>
+        <br></br>
+        <AddDays dates={[this.state.dateQuarter, this.state.daysToBeAdded]} />
+        <DifferenceBetweenDates dates={[this.state.datesArray[0], this.state.datesArray[1]]} />
+        <MaximumDate dates={this.state.datesArray} />
+        <MinimumDate dates={this.state.datesArray} />
+        <QuarterOfYear date={this.state.dateQuarter}/>
+        <DayOfTheWeek date={this.state.dateQuarter} />
         <SearchBox
           placeholder = "Search Monsters..."
           handleChange = {e => this.setState({ searchField: e.target.value})}
---
+++ src/components/datesHandler/dateFormater.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const FormatedDates = (props) => {
+    var d = new Date(props.date);
+    var month = '' + (d.getMonth() + 1);
+    var day = '' + d.getDate();
+    var year = d.getFullYear();
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+    return ([year, month, day].join('-'));
+  }
